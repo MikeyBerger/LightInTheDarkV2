71a66ba4aac0b1c03edc91661ef3262f8f4d368e
MikeyBerger <michaeljudahberger@gmail.com>
2020-11-29 12:44:24 -0800

    Initial Commit

---
+++ LightInTheDarkV3/Assets/LevelSelectionV2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectionV2 : MonoBehaviour
+{
+    public GameObject[] Buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2); /* < Change this int value to whatever your
+                                                             level selection build index is on your
+                                                             build settings */
+
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            if (i + 2 > levelAt)
+                Buttons[i].active = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
