77133c0868df831cd4de3f2babbf2fff5d8180ae
MikeyBerger <michaeljudahberger@gmail.com>
2020-11-29 17:26:04 -0800

    Trouble With LWRP

---
+++ LightInTheDarkV3/Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Vector2 PlayerMovement;
+    Vector2 LightMovement;
+    public float PlayerSpeed;
+    public float LightSpeed;
+    public float JumpForce;
+    private bool IsGrounded;
+    private bool IsJumping;
+    private bool FacingRight = true;
+    private Transform LightObject;
+    private Rigidbody2D RB;
+    private Animator Anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        LightObject = GameObject.FindGameObjectWithTag("Light").GetComponent<Transform>();
+        Anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RB.velocity = new Vector2(PlayerMovement.x, 0) * PlayerSpeed * Time.deltaTime;
+        LightObject.transform.Translate(new Vector2(LightMovement.x, LightMovement.y) * LightSpeed * Time.deltaTime);
+
+        Flip();
+    }
+
+    void Flip()
+    {
+        Vector3 Scale = transform.localScale;
+
+        if (PlayerMovement.x > 0 && !FacingRight || PlayerMovement.x < 0 && FacingRight)
+        {
+            FacingRight = !FacingRight;
+            Scale.x *= -1;
+        }
+
+        transform.localScale = Scale;
+    }
+
+    #region InputActions
+    public void OnMovePlayer(InputAction.CallbackContext ctx)
+    {
+        PlayerMovement = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnMoveLight(InputAction.CallbackContext ctx)
+    {
+        LightMovement = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            IsJumping = true;
+        }
+    }
+    #endregion
+}
